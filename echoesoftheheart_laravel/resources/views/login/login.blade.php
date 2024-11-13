9fff8155967674ffff418477a477cac9cd5cefae
AlbaS-R <albasanchezromero345@gmail.com>
2024-11-13 23:51:28 +0100

    css

--- echoesoftheheart_laravel/resources/views/login/login.blade.php
+++ echoesoftheheart_laravel/resources/views/login/login.blade.php
@@ -1,27 +1,32 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="{{ asset('css/loginStyle.css') }}">
-</head>
-<body>
-    <div class = 'container'>
-        <div id="contenido">
-            <img src="/login/imagenes/logo.png" alt=""/>
-            <h1>Echoes of the heart</h1>
-            <h3>Log in</h3>
-            <br>
-            <p>Usuario /  Email</p>
-            <input type="text">
-            <br>
-            <p>Contraseña</p>
-            <input type="text">
-            <br>
-            <br>
-            <button>Send</button> <button>Registrar</button> <button>Registrar con Google</button>
-        </div>
+@extends('layouts.login-signin')
 
+@section('content')
+<div id="contenido">
+    <br>
+    <br>
+    <img src="{{ asset('images/login/logo.png') }}" style=" width: 250px;" id="logo"/>
+    <div class="contenidor">
+
+        <p>Usuario /  Email</p>
+        <input type="text">
+        <br>
+        <p>Contraseña</p>
+        <input type="text">
     </div>
-</body>
-</html>
+    <br>
+    <div class="botones">
+        <button id="btnSend" type="submit" style="border: none; background: none;">
+            <img src="{{ asset('images/login/botón.png') }}" alt="Enviar" style="width: 150px; height: auto;">
+        </button>
+
+        <button id="btnRgt" type="submit" style="border: none; background: none;">
+            <img src="{{ asset('images/login/botón.png') }}" alt="Registrar" style="width: 150px; height: auto;">
+        </button>
+        <br>
+    <button id="btnRgtGoogle" type="submit" style="border: none; background: none;">
+        <img src="{{ asset('images/login/botón.png') }}" alt="Registrar" style="width: 200px; height: auto;">
+    </button>
+    </div>
+
+</div>
+@endsection
--- echoesoftheheart_laravel/resources/views/login/registro.blade.php
+++ echoesoftheheart_laravel/resources/views/login/registro.blade.php
@@ -1,29 +1,23 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('css/registroStyle.css') }}">
-</head>
-<body>
-    <div class = 'container'>
-        <h1>Echoes of the heart</h1>
-        <h3>Registro</h3>
-        <br>
-        <p>Usuario:</p>
-        <input type="text">
-        <br>
-        <p>Email:</p>
-        <input type="text">
-        <br>
-        <p>Contraseña:</p>
-        <input type="text">
-        <br>
-        <p>Repite la contraseña:</p>
-        <input type="text">
-        <br>
-        <br>
-        <button>Registro</button> <button>Registrar con Google</button>
-    </div>
-</body>
-</html>
+@extends('layouts.login-signin')
+
+@section('content')
+<div id="contenido">
+    <h1>Echoes of the heart</h1>
+    <h3>Registro</h3>
+    <br>
+    <p>Usuario:</p>
+    <input type="text">
+    <br>
+    <p>Email:</p>
+    <input type="text">
+    <br>
+    <p>Contraseña:</p>
+    <input type="text">
+    <br>
+    <p>Repite la contraseña:</p>
+    <input type="text">
+    <br>
+    <br>
+    <button>Registro</button> <button>Registrar con Google</button>
+</div>
+@endsection
