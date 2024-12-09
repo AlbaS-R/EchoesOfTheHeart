eb578a8b66db0b86de1f1f7f31ba2b133399d221
AlbaS-R <albasanchezromero345@gmail.com>
2024-12-09 19:32:49 +0100

    más cosas de inicio

--- echoesoftheheart_laravel/resources/views/inici/principal.blade.php
+++ echoesoftheheart_laravel/resources/views/inici/principal.blade.php
@@ -11,10 +11,30 @@
     <img class="img1" src="{{ asset('images/principal/pilar.png') }}" alt="Imagen Principal">
     <img class="img2"  src="{{ asset('images/principal/pilar.png') }}" alt="Imagen Principal">
     <div class="usuario">
-        <!-- Contenido de usuario -->
+        <div class="user-info">
+            <div class="name">
+                {{ Auth::user()->name }}
+            </div>
+            <div class="stats">
+                <div class="stat-item">
+                    {{ Auth::user()->dolares }}
+                </div>
+                <div class="stat-item">
+                    {{ Auth::user()->esencias }}
+                </div>
+            </div>
+        </div>
     </div>
+
     <div class="contenido">
-        <!-- Contenido principal -->
+        <img class="imgJrrn" src="{{ asset('images/principal/jarrón.png') }}" alt="Jarrón Principal">
+        <img class="imgPrg" src="{{ asset('images/principal/pergaminoMain.png') }}" alt="Pergamino Main">
+        <br>
+        <br>
+        <img class="imgDiv" src="{{ asset('images/principal/divisor1.png') }}" alt="Divisor Principal">
+    </div>
+    <div class="updates">
+
     </div>
 </body>
 </html>
